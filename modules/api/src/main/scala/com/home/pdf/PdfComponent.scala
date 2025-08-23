64b62a1add7e7804b28699193f363bc03768e67d
paq1 <p.paquin1605@gmail.com>
2025-08-23 23:15:09 +0200

    refactor

--- modules/api/src/main/scala/com/home/pdf/PdfComponent.scala
+++ modules/api/src/main/scala/com/home/pdf/PdfComponent.scala
@@ -2,15 +2,11 @@ package com.home.pdf
 
 import com.home.pdf.routers.PdfCompareController
 import com.home.pdf.services.comparator.FileContentComparator
-import com.play.controllers.HealthController
 import play.api.BuiltInComponentsFromContext
 
 trait PdfComponent { self: BuiltInComponentsFromContext =>
 
   lazy val pdfCompareController = new PdfCompareController(fileContentCompartor, controllerComponents)
-  lazy val healthController = new HealthController(controllerComponents)
-
-
 
   private lazy val fileContentCompartor = new FileContentComparator()
 }
