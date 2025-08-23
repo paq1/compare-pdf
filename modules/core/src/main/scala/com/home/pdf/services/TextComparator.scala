3bf83e9df018969b9f5833c308a2f91f1cd2984b
paq1 <p.paquin1605@gmail.com>
2025-08-23 22:59:09 +0200

    refactor

--- modules/core/src/main/scala/com/home/pdf/services/TextComparator.scala
+++ modules/core/src/main/scala/com/home/pdf/services/TextComparator.scala
@@ -1,15 +1,12 @@
 package com.home.pdf.services
 
 
-/**
- *
- */
 trait TextComparator {
 
-  /** Compare ligne par ligne, renvoie (equal, diffs) */
+
   def compareLineByLine(t1: String, t2: String): (Boolean, Seq[LineDiff]) = {
-    val l1 = normalize(t1).split("\n", -1).toVector
-    val l2 = normalize(t2).split("\n", -1).toVector
+    val l1 = sanitize(t1).split("\n", -1).toVector
+    val l2 = sanitize(t2).split("\n", -1).toVector
     val max = math.max(l1.length, l2.length)
     val diffs = (0 until max).flatMap { i =>
       val a = if (i < l1.length) l1(i) else ""
@@ -19,10 +16,9 @@ trait TextComparator {
     (diffs.isEmpty, diffs)
   }
 
-  /** Petites normalisations pour limiter les faux positifs */
-  private def normalize(s: String): String =
-    s.replaceAll("\r\n?", "\n")     // CRLF -> LF
-      .replaceAll("[ \t]+", " ")     // compresser espaces/tabs
+  private def sanitize(s: String): String =
+    s.replaceAll("\r\n?", "\n")
+      .replaceAll("[ \t]+", " ")
       .trim
 
 }
